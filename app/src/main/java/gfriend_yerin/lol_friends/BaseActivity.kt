584b4f68fdce8d27cee83da2e64a4280b3a57633
Girlfirend-Yerin <hsunj2003@naver.com>
2019-07-16 19:23:25 +0900

    Defualt File Setting

---
+++ app/src/main/java/gfriend_yerin/lol_friends/BaseActivity.kt
@@ -0,0 +1,34 @@
+package gfriend_yerin.lol_friends
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    var parseTime = 0L
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
